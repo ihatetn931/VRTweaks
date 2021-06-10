9a9b1d0bf01a38c168e5d22bf143fdff8b802038
Ethan Fischer <ethan.f.fischer@gmail.com>
2021-06-10 12:06:40 -0500

    Gamepad Gaze Cursor (#27)
    * Fix pause menu
    
    * Fix PDA menu and WIP gamepadinputmodule
    
    * Some fixes
    
    * WIP separate gaze cursor for gamepad
    
    * Make crafting menu work
    
    * Separate classes into files
    
    * Quick slot working with gaze/nongaze!
    
    * Get rid of changes in snap turning menu
    
    * Put snapturning options back
    
    * Put csproj back
    
    * Put commondir path back
    
    * Fix namespace
    
    * Fix back button not working in mainoptionsmenu

---
+++ VRTweaks/GamepadGazeCursorFixer/MainMenuOptionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace VRTweaks.GamepadGazeCursorFixer
+{
+    [HarmonyPatch(typeof(MainMenuOptions), nameof(MainMenuOptions.Update))]
+    public static class MainMenuOptionsMenu
+    {
+        public static void Postfix(MainMenuOptions __instance)
+        {
+            if (__instance.tabbedPanel != null && GameInput.GetButtonDown(uGUI.button1))
+            {
+                if (__instance.tabbedPanel.dialog.open)
+                {
+                    __instance.tabbedPanel.dialog.Close();
+                    return;
+                }
+                __instance.OnBack();
+            }
+        }
+    }
+}
---
+++ VRTweaks/GamepadGazeCursorFixer/PDAMenu.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using VRTweaks.GamepadGazeCursorFixer.Utilities;
+
+namespace VRTweaks.GamepadGazeCursorFixer
+{
+    [HarmonyPatch(typeof(uGUI_PDA), "UpdateRaycasterStatus")]
+    public static class PDAMenuRaycaster
+    {
+        public static bool Prefix(ref uGUI_GraphicRaycaster raycaster, uGUI_PDA __instance)
+        {
+            RaycastFixer.Fix(ref raycaster, __instance);
+            return false;
+        }
+    }
+
+
+    static class PDAQuickSlotAssignment
+    {
+        private static bool _didDisableGazeCursor;
+
+        [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnUpdate))]
+        public static class PDAQuickSlotAssignmentBegin
+        {
+            public static void Postfix()
+            {
+                if (VROptions.GetUseGazeBasedCursor() && GameInput.GetButtonDown(uGUI.button3))
+                {
+                    Inventory.main.ExecuteItemAction(ItemDragManager.hoveredItem, 3);
+                    VROptions.gazeBasedCursor = false;
+                    _didDisableGazeCursor = true;
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(QuickSlots), nameof(QuickSlots.EndAssign))]
+        public static class PDAQuickSlotAssignmentEnd
+        {
+            public static void Postfix()
+            {
+                if (_didDisableGazeCursor)
+                {
+                    VROptions.gazeBasedCursor = true;
+                }
+            }
+        }
+
+    }
+}
